1123f6efe981505fa227e64675348f3d2e058c0c
이다현 <74286308+C0RVU55@users.noreply.github.com>
2020-12-09 19:30:18 +0900

    List

--- src/com/javaex/ex02/FriendApp.java
+++ src/com/javaex/ex02/FriendApp.java
@@ -1,13 +1,29 @@
 package com.javaex.ex02;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class FriendApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
+		List<Friend> fList = new ArrayList<Friend>(); // 처음에 <String>했는데 Friend 넣을 거니까 <Friend>
 
-    	
-    	
-    }
+		Scanner sc = new Scanner(System.in);
+		System.out.println("친구를 3명 등록해 주세요.");
+
+		for (int i = 0; i < 3; i++) {
+			String f = sc.nextLine();
+			String[] fArr = f.split(" "); // 매번 값 받아서 나누고 Friend클래스에 대입
+			Friend ff = new Friend(fArr[0], fArr[1], fArr[2]);
+			fList.add(ff);
+		}
+		for (int i = 0; i < fList.size(); i++) {
+			fList.get(i).showInfo();
+		}
+
+		sc.close();
+	}
 
 }
--- src/com/javaex/ex03/GoodsApp.java
+++ src/com/javaex/ex03/GoodsApp.java
@@ -1,10 +1,44 @@
 package com.javaex.ex03;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class GoodsApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-       
-    }
+		int sum = 0;
+
+		Scanner sc = new Scanner(System.in);
+
+		List<Goods> gList = new ArrayList<Goods>();
+
+		System.out.println("상품을 입력해 주세요(종료 q)");
+
+		while (true) {
+			String g = sc.nextLine();
+
+			if (g.equals("q")) {
+				System.out.println("[입력완료]\n===================");
+				break;
+			} else {
+				String[] gArr = g.split(",");
+				Goods goods = new Goods(gArr[0], Integer.parseInt(gArr[1]), Integer.parseInt(gArr[2]));
+				gList.add(goods);
+			}
+		}
+
+		for (int i = 0; i < gList.size(); i++) {
+			gList.get(i).stock();
+		}
+		for (int i = 0; i < gList.size(); i++) {
+			sum=sum+gList.get(i).getCount();
+		}
+		System.out.println("모든 상품의 개수는 "+sum+"개입니다.");
+
+		sc.close();
+
+	}
 
 }
